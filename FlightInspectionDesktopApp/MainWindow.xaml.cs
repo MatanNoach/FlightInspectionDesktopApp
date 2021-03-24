684e6a393d194f8a041848a7d54c6efed617ada9
Spring_Party <Yuval.e@hotmail.com>
2021-03-24 20:29:58 +0200

    Connection to FlightGear

--- FlightInspectionDesktopApp/MainWindow.xaml.cs
+++ FlightInspectionDesktopApp/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace FlightInspectionDesktopApp
 {
     /// <summary>
@@ -21,8 +26,40 @@ namespace FlightInspectionDesktopApp
     public partial class MainWindow : Window
     {
         public MainWindow()
-        {
-            InitializeComponent();
+        {            
+            InitializeComponent();          
+
+            // input from user
+            int playingSpeed = 100;            
+            string csv_file = "C:\\Users\\user\\Documents\\University\\תשפא\\סמסטר ב\\תכנות מתקדם 2\\תרגילים\\Milestone1\\reg_flight.csv";
+
+            // connection string
+            int port = 5400;
+            string hostname = "localhost";
+
+            // Socket for FlightGear
+            IPHostEntry host = Dns.GetHostEntry(hostname);
+            IPEndPoint ipe = new IPEndPoint(host.AddressList[1],port);
+            Socket socket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(ipe);
+            NetworkStream net_socket = new NetworkStream(socket);
+            StreamWriter output = new StreamWriter(net_socket);
+            
+            // Reading data from csv file            
+            StreamReader src = new StreamReader(csv_file);
+            string currentLine;
+            // currentLine will be null when the StreamReader reaches the end of file
+            while ((currentLine = src.ReadLine()) != null)
+            {                
+                output.WriteLine(currentLine);
+                Thread.Sleep(playingSpeed);
+            }
+            
+            // closing streams and sockets
+            src.Close();
+            output.Close();
+            net_socket.Close();
+            socket.Close();
         }
     }
 }
