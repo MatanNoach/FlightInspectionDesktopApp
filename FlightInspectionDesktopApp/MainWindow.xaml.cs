9806b8a6f6fef53403fabb642d30550719fa4999
Spring_Party <Yuval.e@hotmail.com>
2021-03-29 14:50:38 +0300

    added using instead of open and close

--- FlightInspectionDesktopApp/MainWindow.xaml.cs
+++ FlightInspectionDesktopApp/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace FlightInspectionDesktopApp
     {
         public MainWindow()
         {            
-            InitializeComponent();          
+            InitializeComponent();
 
             // input from user
             int playingSpeed = 100;            
@@ -40,26 +40,27 @@ namespace FlightInspectionDesktopApp
             // Socket for FlightGear
             IPHostEntry host = Dns.GetHostEntry(hostname);
             IPEndPoint ipe = new IPEndPoint(host.AddressList[1],port);
-            Socket socket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(ipe);
-            NetworkStream net_socket = new NetworkStream(socket);
-            StreamWriter output = new StreamWriter(net_socket);
-            
-            // Reading data from csv file            
-            StreamReader src = new StreamReader(csv_file);
-            string currentLine;
-            // currentLine will be null when the StreamReader reaches the end of file
-            while ((currentLine = src.ReadLine()) != null)
-            {                
-                output.WriteLine(currentLine);
-                Thread.Sleep(playingSpeed);
-            }
-            
-            // closing streams and sockets
-            src.Close();
-            output.Close();
-            net_socket.Close();
-            socket.Close();
+            using (Socket socket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.Connect(ipe);
+                using (NetworkStream net_socket = new NetworkStream(socket))
+                {
+                    using (StreamWriter output = new StreamWriter(net_socket))
+                    {
+                        // Reading data from csv file            
+                        using( StreamReader src = new StreamReader(csv_file))
+                        {
+                            string currentLine;
+                            // currentLine will be null when the StreamReader reaches the end of file
+                            while ((currentLine = src.ReadLine()) != null)
+                            {                
+                                output.WriteLine(currentLine);
+                                Thread.Sleep(playingSpeed);
+                            }
+                        }
+                    }
+                }            
+            }            
         }
     }
 }
