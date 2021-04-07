7b570da87c9bb4f53d1efc9d7ca1907e77d54307
MatanNoach <61862949+MatanNoach@users.noreply.github.com>
2021-04-07 08:55:13 +0300

    creating AnomalyDetector class

---
+++ FlightInspectionDesktopApp/AnomalyDetector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FlightInspectionDesktopApp
+{
+    class AnomalyDetector
+    {
+        [DllImport("LinearRegressionAnomalyDetector.dll", EntryPoint = "TimeSeriesCaller")]
+        public static extern IntPtr CreateTimeSeries(string csvFileName);
+    }
+}
