cac7bd80c2f5ac60187bfb4d66e51071d6cf9429
noamb <noambeer4@gmail.com>
2021-04-11 15:16:32 +0300

    added compass

--- FlightInspectionDesktopApp/UserControls/Compass.xaml.cs
+++ FlightInspectionDesktopApp/UserControls/Compass.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using FlightInspectionDesktopApp.Metadata;
+using System;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FlightInspectionDesktopApp.UserControls
 {
@@ -21,16 +11,20 @@ namespace FlightInspectionDesktopApp.UserControls
     /// </summary>
     public partial class Compass : UserControl
     {
+        private MetadataViewModel vm;
         public Compass()
         {
             InitializeComponent();
+            this.vm = new MetadataViewModel(MetadataModel.Instance);
+            this.DataContext = this.vm;
         }
     }
+
 
     class ImageSizeConverter : IValueConverter
     {
         /// <summary>
-        /// Converts Elevator [-1,1] values to Joystick values.
+        /// Converts the image's width and returns the half of it.
         /// </summary>
         /// <param name="value">value that we're binded to</param>
         /// <param name="targetType">none</param>
@@ -40,6 +34,65 @@ namespace FlightInspectionDesktopApp.UserControls
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return (double)value / 2;
+        }
+
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class YAxisConverter1 : IValueConverter
+    {
+        /// <summary>
+        /// Converts the image's height.
+        /// </summary>
+        /// <param name="value">value that we're binded to</param>
+        /// <param name="targetType">none</param>
+        /// <param name="parameter">JoystickBoundries Ellipse</param>
+        /// <param name="culture">none</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value / 2 - (double)value / 4;
+        }
+
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class YAxisConverter2 : IValueConverter
+    {
+        /// <summary>
+        /// Converts the image's height.
+        /// </summary>
+        /// <param name="value">value that we're binded to</param>
+        /// <param name="targetType">none</param>
+        /// <param name="parameter">JoystickBoundries Ellipse</param>
+        /// <param name="culture">none</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value / 2 + (double)value / 4;
         }
 
         /// <summary>
