17a8a50b2d1d2f41fcadeb4555774a442670a2ac
noamb <noambeer4@gmail.com>
2021-04-11 16:12:27 +0300

    added yaw control

---
+++ FlightInspectionDesktopApp/UserControls/Yaw.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FlightInspectionDesktopApp.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightInspectionDesktopApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Yaw.xaml
+    /// </summary>
+    public partial class Yaw : UserControl
+    {
+        private MetadataViewModel vm;
+        public Yaw()
+        {
+            InitializeComponent();
+            this.vm = new MetadataViewModel(MetadataModel.Instance);
+            this.DataContext = this.vm;
+        }
+    }
+}
