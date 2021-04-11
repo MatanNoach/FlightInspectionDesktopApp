5f97d2d6849620dfde18ff60025fe4d34c2363fc
noamb <noambeer4@gmail.com>
2021-04-11 13:24:29 +0300

    started creating a compass

---
+++ FlightInspectionDesktopApp/UserControls/Compass.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightInspectionDesktopApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Compass.xaml
+    /// </summary>
+    public partial class Compass : UserControl
+    {
+        public Compass()
+        {
+            InitializeComponent();
+        }
+    }
+
+    class ImageSizeConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts Elevator [-1,1] values to Joystick values.
+        /// </summary>
+        /// <param name="value">value that we're binded to</param>
+        /// <param name="targetType">none</param>
+        /// <param name="parameter">JoystickBoundries Ellipse</param>
+        /// <param name="culture">none</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value / 2;
+        }
+
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
